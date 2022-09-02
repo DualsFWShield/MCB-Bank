a52774972471ded58389e99bb846c6c7c987ac18
Noah Bruijninckx <66059559+DiamsLabs543@users.noreply.github.com>
2022-09-02 09:38:45 +0200

    Update db.php

--- assets/db.php
+++ assets/db.php
@@ -1,7 +1,8 @@
 <?php
     $con = new mysqli('localhost','root','','mybank');
     define('bankName', 'MCB Bank',true);
-    $ar = $con->query("select * from userAccounts, branch where id = '$_SESSION[userId]' AND userAccounts.branch = branch.branchId"); 
+    $ar = $con->query("select * from userAccounts, branch where id = '$_SESSION[userId]' AND userAccounts.branch = branch.branchId");
+    # Undefined array key “userId” in C:\xampp\htdocs\bank\assets\db.php on line 4
     $userData = $ar->fetch_assoc();
 ?>
 <script type="text/javascript">
